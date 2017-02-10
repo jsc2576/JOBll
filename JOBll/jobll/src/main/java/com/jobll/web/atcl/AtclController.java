b3b621ca95be34298706b2363241784d4f17c4b5
정수찬 <jsc2576@gmail.com>
2017-02-10 13:22:01 +0900

    make atcl controller

---
+++ JOBll/jobll/src/main/java/com/jobll/web/atcl/AtclController.java
@@ -0,0 +1,33 @@
+package com.jobll.web.atcl;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class AtclController {
+
+	@RequestMapping("/atcl/all")
+	public String AtclAll(){
+		return "AtclAll";
+	}
+	
+	@RequestMapping("/atcl/rcv/wait")
+	public String AtclRcvWait(){
+		return "AtclRcvWait";
+	}
+	
+	@RequestMapping("/atcl/rcv/cmpt")
+	public String AtclRcvCmpt(){
+		return "AtclRcvWait";
+	}
+	
+	@RequestMapping("/atcl/prcs/wait")
+	public String AtclPrcsWait(){
+		return "AtclPrcsWait";
+	}
+	
+	@RequestMapping("/atcl/prcs/cmpt")
+	public String AtclPrcsCmpt(){
+		return "AtclPrcsCmpt";
+	}
+}
---
+++ JOBll/jobll/src/main/java/com/jobll/web/home/Home.java
@@ -0,0 +1,28 @@
+package com.jobll.web.home;
+
+import java.io.Serializable;
+import java.util.List;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Repository;
+
+
+@Repository("home")
+public class Home implements Serializable {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1499476260178885152L;
+	/**
+	 * 
+	 */
+	
+	private Integer test_idx;
+	public Integer getTest_idx() {
+		return test_idx;
+	}
+	public void setTest_idx(Integer test_idx) {
+		this.test_idx = test_idx;
+	}
+	
+}
