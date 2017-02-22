e0e193f49c2d57559c539b7a30fc026327e17e9c
OhSeongCheol <dhtmdcjf7@gmail.com>
2017-02-22 21:23:30 +0900

    modify signup session url

--- jobll/src/main/java/com/jobll/web/WebSecurityConfig.java
+++ jobll/src/main/java/com/jobll/web/WebSecurityConfig.java
@@ -62,7 +62,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .antMatchers("/signin/**").permitAll()
             	.antMatchers("/resources/**").permitAll()
             	.antMatchers("/login*","/signin/**","/signup/**").permitAll()
-            	.antMatchers("/usrInfoJoin").permitAll()
+            	.antMatchers("/usrInfo/usrInfoJoin/go").permitAll()
             	//.antMatchers("/**").permitAll()
                 .anyRequest().authenticated()
                 .and()
