6ce942c832cb20aa117125d0de4272e7f28660fd
nildream <nildream@naver.com>
2017-03-26 21:23:58 +0900

    복구

--- jobll/src/main/java/com/jobll/web/JobllApplication.java
+++ jobll/src/main/java/com/jobll/web/JobllApplication.java
@@ -8,9 +8,7 @@ import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry
 public class JobllApplication {
 
 	public static void main(String[] args) {
-		//System.out.println("scTest");
 		SpringApplication.run(JobllApplication.class, args);
-		//System.out.println("scTest");
 	}
 
 }
--- jobll/src/main/webapp/WEB-INF/views/jsp/dashboard/scriptList.jsp
+++ jobll/src/main/webapp/WEB-INF/views/jsp/dashboard/scriptList.jsp
@@ -1,2 +1,2 @@
 <%@ page contentType="text/html; charset=UTF-8" pageEncoding="UTF-8"%>
-<script src="/resources/js/dashboard/dashboardList.js" type="text/javascript"></script>
+<script src="/resources/js/atclInfo/atclInfoList/atclInfoList.js" type="text/javascript"></script>
