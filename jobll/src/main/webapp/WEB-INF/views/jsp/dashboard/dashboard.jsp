0e81a89ef559a3d88b9fba7043efcb897494086a
k401lb <jsc2576@gmail.com>
2017-05-03 16:36:26 +0900

    대시보드 기능 간추림

--- jobll/src/main/webapp/WEB-INF/views/jsp/dashboard/dashboard.jsp
+++ jobll/src/main/webapp/WEB-INF/views/jsp/dashboard/dashboard.jsp
@@ -1,36 +1,12 @@
 <div class="row">
-	<div class="col-md-6">
-	<div id="cunt_list"></div>
-	</div>
-	
-	<div class="col-md-6">
+	<div class="col-md-2">
 	<div id="cnet_list_name"></div>
 	<select id="cnet_list"></select>
 	</div>
 </div>
 
 <div class="row">
-	<div class="col-md-2"></div>
 	<div class="col-md-8">
 		<div id="Nwagon"></div>
-	</div>
-	<div class="col-md-2"></div>
-</div>
-
-<div class="row">
-	<div class="col-md-6">
-		<table>
-		<thead>
-		<tr>
-			<th>num</th>
-			<th>title</th>
-			<th>údate</th>
-			<th>êauthor</th>
-		</tr>
-		</thead>
-		<tbody>
-			
-		</tbody>
-		</table>
 	</div>
 </div>
--- jobll/src/main/webapp/resources/js/dashboard/dashboard.js
+++ jobll/src/main/webapp/resources/js/dashboard/dashboard.js
@@ -1,6 +1,5 @@
 $(document).ready(function(){
 	Cunt_Prjt();
-	Cnet_Prjt();
 	Tot_Prcs();
 	Cnet_Prjt_Change();
 });
@@ -9,10 +8,23 @@ var cunt_prjt;
 
 function Cunt_Prjt(){ // 최근 프로젝트
 	
+	$("#cnet_list_name").html("프로젝트 리스트");
 	$.ajax({
 		method: "POST",
 		url: "/cuntPrjt/check/read",
 		success:function(list){
+			var str_html = "<option value=''>----최근----</option>";
+			$.each(list, function(index, data){
+				str_html += "<option value='";
+				str_html += data.prjt_idx;
+				str_html += "'>";
+				str_html += data.prjt_sbjt;
+				str_html += "</option>";
+			});
+			
+			Cnet_Prjt(str_html);
+			//$("#cnet_list").html(str_html);
+			/*
 			var str_html = "<table class = 'cunt_prjt' border='1'>";
 			str_html += "<thead><tr>";
 			str_html += "<th>최근 프로젝트</th>";
@@ -28,7 +40,7 @@ function Cunt_Prjt(){ // 최근 프로젝트
 			
 			
 			str_html += "</tbody> </table>";
-			$("#cunt_list").html(str_html);
+			$("#cunt_list").html(str_html);*/
 		},
 		error: function(){
 			var str_html = "<table class = 'cunt_prjt'>";
@@ -42,15 +54,14 @@ function Cunt_Prjt(){ // 최근 프로젝트
 	});
 }
 
-function Cnet_Prjt(){ // 담당 프로젝트
+function Cnet_Prjt(str_html){ // 담당 프로젝트
 	
-	$("#cnet_list_name").html("담당 프로젝트");
 	
 	$.ajax({
 		method: "POST",
 		url: "/cnetPrjt/check/read",
 		success: function(list){
-			var str_html = "<option value=''></option>";
+			str_html += "<option value=''>----담당----</option>";
 			$.each(list, function(index, data){
 				str_html += "<option value='";
 				str_html += data.prjt_idx;
@@ -99,11 +110,11 @@ function Circle_chart(tot, rmn, end){
 				colorset: ['#2EB400', '#2BC8C9', "#666666"],
 				fields: ['종료 이슈', '진행 이슈',  '남은 이슈'],
 			},
-			'donut_width' : 35,
-			'core_circle_radius':50,
+			'donut_width' : 100,
+			'core_circle_radius':0,
 			'chartDiv': 'Nwagon',
-			'chartType': 'donut',
-			'chartSize': {width:700, height:400}
+			'chartType': 'pie',
+			'chartSize': {width:500, height:200}
 		};
 	$("#Nwagon").html("");
 	Nwagon.chart(options);	
