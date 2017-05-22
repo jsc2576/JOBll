6c86fd9e35d19290cbfcc46c4d33de4b6f18dd03
OhSeongCheol <dhtmdcjf7@gmail.com>
2017-05-22 15:16:26 +0900

    유저리스트 새로운 템플릿 적용, 기본프프로필사진 변경

--- jobll/src/main/webapp/WEB-INF/views/jsp/usrInfo/usrInfoList/usrInfoList.jsp
+++ jobll/src/main/webapp/WEB-INF/views/jsp/usrInfo/usrInfoList/usrInfoList.jsp
@@ -1,10 +1,10 @@
 <%@ page contentType="text/html; charset=UTF-8" pageEncoding="UTF-8"%>
 
-<jsp:include page="../../usrInfo/usrInfoList/usrInfoMenu.jsp" flush="false" />
+<!--<jsp:include page="../../usrInfo/usrInfoList/usrInfoMenu.jsp" flush="false" />  -->
 
 <!-- data table -->
  	<div id="list"></div>
- 		
+
  <div class="jb-center">
   	<ul class="pagination" id="page_nm">
 	</ul>
--- jobll/src/main/webapp/resources/js/usrInfo/usrInfoList/usrInfoList.js
+++ jobll/src/main/webapp/resources/js/usrInfo/usrInfoList/usrInfoList.js
@@ -9,7 +9,7 @@ function usrList() {
 			url		: "/usrInfo/allPersonInfo/check/read",
 			
 			success	: function (list) {
-				var str_html = "<form id='usrInfoRead' action = '/usrInfo/MyUsrInfo' method='get'>";
+				/*var str_html = "<form id='usrInfoRead' action = '/usrInfo/MyUsrInfo' method='get'>";
 				str_html += "<table class = 'table'>";
 				str_html += "<thead><tr>";
 				str_html += "<th class = 'tb-typ'>번호</th>";
@@ -37,8 +37,45 @@ function usrList() {
 				str_html += "</tbody>";
 				str_html += "</table>";
 				str_html += "<div id = 'getIdx'></div>";
+				str_html += "</form>";*/
+				
+				
+				var str_html = "<form id='usrInfoRead' action = '/usrInfo/MyUsrInfo' method='get'>";
+				str_html += " <div class=\"row\">";
+				str_html += "<div class=\"col-lg-12\">";
+				str_html += "<h1 class=\"page-header\">회원 목록</h1></div></div>";
+
+				str_html += "<div class=\"row\"><div class=\"col-lg-12\"><div class=\"panel panel-default\"><div class=\"panel-heading\">";
+				str_html += "전체 회원 목록 리스트";
+				str_html += "</div><div class=\"panel-body\">";
+				str_html += "<table width=\"80%\" class=\"table table-striped table-bordered table-hover\" id=\"dataTables-example\">";
+				str_html += "<thead><tr>";
+				str_html +="<th>번호</th>";
+				str_html += "<th>이름</th>";
+				str_html +="<th>등급</th>";
+				str_html +="<th>기관</th>";
+				str_html +="<th width=10%> </th>";
+				str_html +="</tr></thead><tbody>";
+				
+		
+					
+				$.each(list, function(index, value){
+					str_html += "<tr>";
+					str_html += "<td>"+(index+1)+"</td>";
+					str_html += "<td>"+value.usr_nm+"</td>";
+					str_html += "<td>"+value.usr_lv+"</td>";
+					str_html += "<td>"+value.usr_pwd+"</td>";
+					str_html += "<td>"+"<button onclick='submit()'  name = 'usr_id'";
+					
+					str_html += " id = 'usr_id'";
+					str_html += " value = '"+value.usr_id+"'";
+					str_html += ">상세열람</button>"+"</td>";
+					
+					str_html += "</a></tr>";
+					
+				});	
+				str_html +="</tbody></table></div></div></div></div>";
 				str_html += "</form>";
-			
 				$("#list").html(str_html);
 			},
 			complete	: function () {
